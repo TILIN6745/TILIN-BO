1d8809cd4b68ced1647b1a2b42cb61defa7f9ff7
Tilin <jm293229@gmail.com>
2025-11-13 21:05:29 -0600

    Update owner phone number in config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ global.isBaileysFail = false;
 global.defaultLenguaje = 'es';
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
+  ['5219', '👑 Mystic - Creador 👑', true],
   ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
   ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
   ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
